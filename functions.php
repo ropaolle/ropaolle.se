aa83147c9e1f7cc86c99626411ec3f3c5556f8d5
ropaolle <ropaolle@gmail.com>
2018-02-14 08:38:04 +0100

    Theme name and less

--- functions.php
+++ functions.php
@@ -15,11 +15,3 @@ function beans_child_enqueue_uikit_assets() {
 
 }
 
-// Remove this action and callback function if you are not adding CSS in the style.css file.
-add_action( 'wp_enqueue_scripts', 'beans_child_enqueue_assets' );
-
-function beans_child_enqueue_assets() {
-
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
-
-}
